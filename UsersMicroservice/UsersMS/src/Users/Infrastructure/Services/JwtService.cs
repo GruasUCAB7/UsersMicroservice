6cc97df1b860a9b3d19ede36157bc5425f7c16aa
AndresRagua <andrecitoraguag@gmail.com>
2025-01-07 01:03:41 -0400

    authcontroller update - JWT limitedGenerator

--- UsersMicroservice/UsersMS/src/Users/Infrastructure/Services/JwtService.cs
+++ UsersMicroservice/UsersMS/src/Users/Infrastructure/Services/JwtService.cs
@@ -10,12 +10,15 @@ namespace UsersMS.src.Users.Infrastructure.Services
     {
         private readonly IConfiguration _configuration = configuration;
 
-        public string GenerateToken(string userId, string email, string role)
+        public string GenerateToken(string userId, string name, string email, string phone, bool isTemporaryPassword, string role)
         {
             var claims = new[]
             {
             new Claim(JwtRegisteredClaimNames.Sub, userId),
+            new Claim(JwtRegisteredClaimNames.Name, name),
             new Claim(JwtRegisteredClaimNames.Email, email),
+            new Claim(JwtRegisteredClaimNames.PhoneNumber, phone),
+            new Claim("isTemporaryPassword", isTemporaryPassword.ToString().ToLower()),
             new Claim(ClaimTypes.Role, role),
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
         };
@@ -29,6 +32,30 @@ namespace UsersMS.src.Users.Infrastructure.Services
                 audience: Env.GetString("JWT_AUDIENCE"),
                 claims: claims,
                 expires: DateTime.UtcNow.AddMinutes(int.Parse(Env.GetString("JWT_EXPIRES_IN_MINUTES"))),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public string GenerateLimitedToken(string userId, string email)
+        {
+            var claims = new[]
+            {
+        new Claim(JwtRegisteredClaimNames.Sub, userId),
+        new Claim(JwtRegisteredClaimNames.Email, email),
+        new Claim("isLimitedToken", "true"),
+        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+    };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Env.GetString("JWT_KEY")));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: Env.GetString("JWT_ISSUER"),
+                audience: Env.GetString("JWT_AUDIENCE"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(15),
                 signingCredentials: credentials
             );
 
--- UsersMicroservice/UsersMS/src/Users/Infrastructure/Types/UserChangePasswordRequest.cs
+++ UsersMicroservice/UsersMS/src/Users/Infrastructure/Types/UserChangePasswordRequest.cs
@@ -2,8 +2,9 @@ namespace UsersMS.src.Users.Infrastructure.Types
 {
     public class UserChangePasswordRequest
     {
-        public required string Email { get; set; }
-        public required string OldPassword { get; set; }
-        public required string NewPassword { get; set; }
+        public string Email { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
     }
+
 }
