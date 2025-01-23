6e5d6c5bad6c3296a8a808d330e6d1467c6d95f7
Joyce <joywg14@gmail.com>
2025-01-23 00:30:02 -0400

    notificacionToken[B46]: Added notification token

---
+++ UsersMicroservice/UsersMS/src/Users/Infrastructure/Validators/AddNotificationTokenValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using UsersMS.src.Users.Application.Commands.AddNotificationToken.Types;
+
+namespace UsersMS.src.Users.Infrastructure.Validators
+{
+    public class AddNotificationTokenValidator : AbstractValidator<AddNotificationTokenCommand>
+    {
+        public AddNotificationTokenValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotEmpty().WithMessage("User ID is required.");
+
+            RuleFor(x => x.Token)
+                .NotEmpty().WithMessage("Token is required.");
+        }
+
+    }
+}
